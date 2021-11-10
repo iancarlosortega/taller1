116cbc138f03cdbb8fee11f18589c49cd9cb2934
Ian Carlos Ortega <iancarlosortegaleon@gmail.com>
2021-11-10 18:48:50 -0500

    Algoritmo de ordenamiento y validador de cedulas.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -20,6 +20,12 @@ class MainActivity : AppCompatActivity() {
         //Pregunta 4
         presentarVehiculo()
 
+        //Algoritmo de ordenamiento
+        metodoBurbuja()
+
+        //Validador de cedula
+        validarCedula()
+
     }
 
     private fun validarEdad(){
@@ -62,6 +68,9 @@ class MainActivity : AppCompatActivity() {
         //Juntar todos los grupos
         var listadoEstudiantes = grupo1 + grupo2 + grupo3
 
+        //Ordenar lista
+        listadoEstudiantes = listadoEstudiantes.sorted()
+
         println("Listado completo de estudiantes:  $listadoEstudiantes")
         println("Grupo 1: $grupo1")
         println("Grupo 2: $grupo2")
@@ -74,5 +83,98 @@ class MainActivity : AppCompatActivity() {
         var vehiculoEjemplo = Vehiculo( traccion = "4x4", motor = "Motor diesel", tipo = "carro", capacidad = 5  )
         vehiculoEjemplo.propiedades()
 
+    }
+
+    private fun metodoBurbuja() {
+
+        var listaNumeros = arrayListOf<Int>(6,2,76,4,1,8,24,90,3)
+
+        //Mostrar lista original
+        println("Lista original: $listaNumeros")
+
+        //Ordenamiento por metodo burbuja
+
+        for ( i in (0 until listaNumeros.size - 1) ) {
+
+            for ( j in (0 until listaNumeros.size - 1) ) {
+
+                if ( listaNumeros[j+1] < listaNumeros[j] ){
+
+                    var aux = listaNumeros[j+1]
+                    listaNumeros[j+1] = listaNumeros[j]
+                    listaNumeros[j] = aux
+
+                }
+
+            }
+
+        }
+
+        //Mostrar resultado
+        println("Lista ordenada: $listaNumeros")
+
+    }
+
+    private fun validarCedula() {
+
+        var cedula = "1104076797"
+        //var cedula = "1150237046"
+        //var cedula = "1104648496"
+        //var cedula = "1150728036"
+        //var cedula = "1104576797"
+        var resultados = arrayListOf<Int>()
+        var total = 0
+
+        for( i in cedula.indices ){
+
+            var resultado = 0
+
+            if ( (i % 2) == 0 ){
+                resultado = cedula[i].toString().toInt() * 2
+            } else {
+                resultado = cedula[i].toString().toInt() * 1
+            }
+
+            if ( resultado > 9 ) {
+                resultado = numerosMayores(resultado)
+            }
+
+            resultados.add(resultado)
+
+        }
+
+        for ( i in (0 until resultados.size - 1) ) {
+            total += resultados[i]
+        }
+
+        //Validar ultimo numero de cedula
+        var ultimoNumero = resultados[resultados.lastIndex]
+        var inmediatoSuperior = (Math.ceil(total/10.0) * 10).toInt()
+
+        var resultadoComparar = inmediatoSuperior - total
+
+        if( ultimoNumero == resultadoComparar ){
+            println("El número de cédula $cedula es válido")
+        } else {
+            println("El número de cédula $cedula no es válido")
+        }
+
+    }
+
+    private fun numerosMayores( numero: Int ): Int {
+
+        var resultado = 0
+
+        var numeroSeparado = numero.toString().split("") as ArrayList<String>
+        numeroSeparado.removeFirst()
+        numeroSeparado.removeLast()
+
+        for ( numero in numeroSeparado ) {
+
+            resultado += numero.toInt()
+
+        }
+
+        return resultado
     }
 }
